10ff034d117b6bba7a0cb73e7aca2f05c564ef21
Daniel Dao <daniel.vu.dao@gmail.com>
2014-07-10 14:52:28 -0500

    currently need to generate random text

--- src/com/classycoder/AlarmReceiver.java
+++ src/com/classycoder/AlarmReceiver.java
@@ -1,9 +1,17 @@
 package com.classycoder;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.SmsManager;
+import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
@@ -13,7 +21,7 @@ public class AlarmReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		/* Receive the number from another intent */
 		String num = intent.getStringExtra("phoneNum");
-		String message = intent.getStringExtra("textBody");		
+		String message = getTextBody(context, Calendar.AM_PM);	
 		
 		/* Parcel read error so need to fix null exception */
 		if(num != null && message != null)
@@ -25,6 +33,39 @@ public class AlarmReceiver extends BroadcastReceiver {
 		/* Send the text */
 		SmsManager messenger = android.telephony.SmsManager.getDefault();
 		messenger.sendTextMessage(num, null, message, null, null);
+	}
+	
+	/* Method to return the text message that needs to be sent */
+	private String getTextBody(Context context, int time_of_day){
+		ArrayList<String> all_txt = new ArrayList<String>();
+		try{
+			BufferedReader br;
+			
+			/* Morning messages */
+			if(time_of_day == 0){
+				br = new BufferedReader(new InputStreamReader(context.getAssets().open("mornings.txt")));
+			}
+			
+			/* Afternoon messages */
+			else{
+				br = new BufferedReader(new InputStreamReader(context.getAssets().open("afternoons.txt")));
+			}
+			
+			String line; 
+			while ((line = br.readLine()) != null){
+				all_txt.add(line);
+			}
+		}
+		/* Error message handling */
+		catch (IOException e){
+			Toast.makeText(context.getApplicationContext(), "Text body cannot be sent.", Toast.LENGTH_SHORT).show();
+		}
+		
+		int len = all_txt.size();
+		Random rand = new Random();
+		
+		/* Pick a random message from the file */
+		return all_txt.get(rand.nextInt(len));
 	}
 	
 }
